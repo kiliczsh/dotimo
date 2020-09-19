d836780c25a875518674e964ffad3116f2bd0c60
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2020-09-19 10:58:29 +0300

    Implemented multi layer application: Data-Core-Business-Application

---
+++ dotimo.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotimo.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
